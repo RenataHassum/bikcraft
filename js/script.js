b9bf0607e7faf5095667fcc844e7335d55580c4d
Renata Hassum <renatahassum@gmail.com>
2022-01-18 13:36:04 -0300

    finalização

--- js/script.js
+++ js/script.js
@@ -23,3 +23,21 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// galeria imagens //
+const galeria = document.querySelectorAll(".bicicletas-comprar-imagens, img");
+const galeriaContainer = document.querySelector(".bicicletas-comprar-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px").matches;
+  if(media) {
+galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach (eventosGaleria);
